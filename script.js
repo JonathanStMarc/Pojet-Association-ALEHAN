9fd232923d809318ce960e0492f2fcb211b28158
jonathansttmarc <jonathanstmarc@gmail.com>
2022-12-04 17:21:16 +0100

    add singification of logo

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let BtnAS = document.getElementById("btnActionSolidaire");
 BtnAS.addEventListener("click", function () {
     let detailAS = document.querySelector(".detailAS");
@@ -7,8 +6,7 @@ BtnAS.addEventListener("click", function () {
     pDetail.textContent =
         "L’action solidaire est une action qui vise à aider les personnes en difficulté. Elle est souvent menée par des associations caritatives ou des ONG. Elle peut être financière, matérielle ou humaine. Elle peut être menée à l’échelle locale, nationale ou internationale. Elle peut être menée par des particuliers ou des entreprises.";
     detailAS.appendChild(pDetail);
-    pDetail.classList.add("border");
-    pDetail.classList.add("border-danger");
+    pDetail.classList.add("border", "border-danger", "border-3", "rounded-4");
 });
 
 let btnAI = document.getElementById("btnActionInsertion");
@@ -19,8 +17,7 @@ btnAI.addEventListener("click", function () {
     pDetail.textContent =
         "L’action d’insertion est une action qui vise à aider les personnes en difficulté à retrouver un emploi ou à se former pour trouver un emploi. Elle est souvent menée par des associations caritatives ou des ONG. Elle peut être financière, matérielle ou humaine. Elle peut être menée à l’échelle locale, nationale ou internationale. Elle peut être menée par des particuliers ou des entreprises.";
     detailAI.appendChild(pDetail);
-    pDetail.classList.add("border");
-    pDetail.classList.add("border-success");
+    pDetail.classList.add("border", "border-success", "border-3", "rounded-4");
 });
 
 let btnAP = document.getElementById("btnActionPrevention");
@@ -31,6 +28,5 @@ btnAP.addEventListener("click", function () {
     pDetail.textContent =
         "L’action de prévention est une action qui vise à éviter que les personnes ne soient en difficulté. Elle est souvent menée par des associations caritatives ou des ONG. Elle peut être financière, matérielle ou humaine. Elle peut être menée à l’échelle locale, nationale ou internationale. Elle peut être menée par des particuliers ou des entreprises.";
     detailAP.appendChild(pDetail);
-    pDetail.classList.add("border");
-    pDetail.classList.add("border-info");
+    pDetail.classList.add("border", "border-info", "border-3", "rounded-4");
 });
